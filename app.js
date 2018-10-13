b89cbdd13778c0faf453bad012a44413d5be3ad5
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2018-10-13 20:16:41 +0530

    Console Work

---
+++ app.js
@@ -0,0 +1,18 @@
+//Log to Console
+console.log('JS I am Comming');
+console.log(123);
+console.log(true);
+var greeting='Hello js';
+console.log(greeting);
+console.log([1,2,23,5]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is Some Error');
+console.clear();
+console.warn('This is Warn Message');
+console.time('hello');
+console.log('JS I am Comming');
+console.log('JS I am Comming');
+console.log('JS I am Comming');
+console.timeEnd('hello');
